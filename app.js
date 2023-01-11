15bcec3d80b5d6487598f77b2d96102f22d16ac9
Sachini Buddhika <sachiniste3@gmail.com>
2023-01-11 17:22:41 +0530

    Add template engine, static pages and set port

---
+++ app.js
@@ -0,0 +1,12 @@
+var express=require('express');
+var app=express();
+
+//set up template engine
+app.set('view engine','ejs');
+
+//static files
+app.use(express.static('./assets'));
+
+//listen to port
+app.listen(3000);
+console.log('you are listening to port 3000');
